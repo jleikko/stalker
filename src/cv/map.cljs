adcc3dce367b7ccd92df2d2a90277e250ac43e37
Jaakko Leikko <jaakko@leikko.net>
2016-10-08 02:32:14 +0300

    marker listener done

--- src/cv/map.cljs
+++ src/cv/map.cljs
@@ -1,4 +1,5 @@
-(ns cv.map)
+(ns cv.map
+  (:require [cv.ds :refer [subscribe-locations]]))
 
 (defn get-channel [element url]
   (.connect js/OskariRPC element url))
@@ -7,14 +8,20 @@
   (.postRequest channel "MapModulePlugin.AddMarkerRequest" (array (js-obj "x" x "y" y
     "size" 3 "color" color) id)))
 
-(defn map-clicked [channel data]
-  (.log js/console data)
-  (add-marker channel "click" (.-lon data) (.-lat data) "ff0000"))
+;;(defn map-clicked [channel data]
+;;  (.log js/console data)
+;;  (add-marker channel "click" (.-lon data) (.-lat data) "ff0000"))
 
 (defn map-ready [channel]
   (.log channel "Map ready")
-  (add-marker channel "ukkeli" 384717 6682960)
-  (.handleEvent channel "MapClickedEvent" (partial map-clicked channel)))
+  ;;(add-marker channel "ukkeli" 384717 6682960)
+  (subscribe-locations "123" (partial handle-location-event channel)))
+  ;;(.handleEvent channel "MapClickedEvent" (partial map-clicked channel)))
+
+
+(defn handle-location-event [channel data]
+  (add-marker channel "ukkeli" (:lon data) (:lat data) "ff0000"))
+
 
 (defn init-map [element url]
     (let [channel (get-channel element url)]
