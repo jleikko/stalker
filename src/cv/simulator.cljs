4467a4eb4b1db8a029c031334ce29be88df27209
Kimmo Laakso <kimmo.laakso@maanmittauslaitos.fi>
2016-10-08 01:47:05 +0300

    simulator

--- src/cv/simulator.cljs
+++ src/cv/simulator.cljs
@@ -1,12 +1,16 @@
 (ns cv.simulator
   (:require [cv.button :refer [button]]
-            [cv.state :refer [counter
-                              increase-count!
-                              decrease-count!
-                              projects
-                              consultants]]
-            [cv.dropdown :refer [dropdown]]
-            [cv.input :refer [input]]))
+            [cv.ds :as ds]))
+
 
 (defn simulator []
-  [:div "JESSE"])
+  [:div (button "start" "start-simulator" start-simulator)])
+
+
+(defn start-simulator []
+  (js/setTimeout send-location 250))
+
+(defn send-location [lat lon]
+  (send-location lat (+ lon 50)))
+
+
