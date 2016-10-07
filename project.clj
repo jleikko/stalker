f2c3c9ae5236f9536f6a787c57a4f3834da657f8
Kimmo Laakso <kimmo.laakso@maanmittauslaitos.fi>
2016-10-07 21:41:54 +0300

    clojurescript version upgrade

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
   :min-lein-version "2.6.1"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/clojurescript "1.9.229"]
                  [org.clojure/core.async "0.2.374"
                   :exclusions [org.clojure/tools.reader]]
                  [reagent "0.5.1":exclusions [cljsjs/react]]
