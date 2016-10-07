1f3802c10f76b19cc3acb0fa39d3e7a7dcd0d230
Jaakko Leikko <jaakko@leikko.net>
2016-10-07 23:27:01 +0300

    clojurefy oskari stuff

--- src/cv/core.cljs
+++ src/cv/core.cljs
@@ -2,33 +2,32 @@
   (:require [reagent.core :as r]
             [cv.app :refer [app]]))
 
-(r/render-component [app]
-                          (. js/document (getElementById "app")))
-
-(def channelGlobal (.connect js/OskariRPC
-    (.getElementById js/document "oskari-map") 
+(defn get-channel []
+  (.connect js/OskariRPC
+    (.getElementById js/document "oskari-map")
     "http://www.paikkatietoikkuna.fi/published/fi/e8784853-1e4f-4644-9d00-5a3f2943f1e6"))
 
-(.log channelGlobal "Channel present")
-;;(.log channelGlobal (.-VERSION js/OskariRPC))
+(defn add-marker [channel id x y color]
+  (.postRequest channel "MapModulePlugin.AddMarkerRequest" (array (js-obj "x" x "y" y
+    "size" 3 "color" color) id)))
 
+(defn map-clicked [channel data]
+  (.log js/console data)
+  (add-marker channel "click" (.-lon data) (.-lat data) "ff0000"))
 
-(.onReady channelGlobal #(mapReady channelGlobal))
-
-(defn mapReady [channel]
+(defn map-ready [channel]
   (.log channel "Map ready")
-  (addMarker "ukkeli" 384717 6682960)
-  (.handleEvent channel "MapClickedEvent" mapClicked)
-)
+  (add-marker channel "ukkeli" 384717 6682960)
+  (.handleEvent channel "MapClickedEvent" (partial map-clicked channel)))
 
-(defn mapClicked [data]
-  (.log channelGlobal data)
-  (addMarker "click" (.-lon data) (.-lat data) "ff0000")
-)
+(defn init-oskari []
+    (let [channel (get-channel)]
+    (.onReady channel #(map-ready channel))))
 
-;; to get current position in browser console run:
-;; window.cv.core.channelGlobal.getMapPosition(console.log)
-(defn addMarker [id x y color]
-  (.postRequest channelGlobal "MapModulePlugin.AddMarkerRequest" (array (js-obj "x" x "y" y 
-    "size" 3 "color" color) id))
-)
+(defn init []
+  (r/render-component [app] (. js/document (getElementById "app")))
+  (init-oskari))
+
+
+(init)
+
