47f56305774d852fd1525444abba1330b4e6a4d2
ZakarFin <zakarfin@gmail.com>
2016-10-07 21:49:41 +0300

    Connect call and simple onready

--- src/cv/core.cljs
+++ src/cv/core.cljs
@@ -4,3 +4,11 @@
 
 (r/render-component [app]
                           (. js/document (getElementById "app")))
+
+(def channel (.connect js/OskariRPC
+    (.getElementById js/document "oskari-map") 
+    "http://www.paikkatietoikkuna.fi/published/fi/e8784853-1e4f-4644-9d00-5a3f2943f1e6"))
+
+(.log channel "Channel present")
+
+(.onReady channel #(.log channel "Map ready"))
